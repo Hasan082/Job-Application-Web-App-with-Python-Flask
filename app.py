0c0738c75da1eb6521d85a31cab14abd3141071b
Md Hasanuzzaman <dr.has82@gmail.com>
2024-06-02 23:02:32 +0600

    [ADDED] Post method added and get all filed data

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
+    if request.method == "POST":
+        first_name = request.form.get("first_name")
+        last_name = request.form.get("last_name")
+        email = request.form.get("email")
+        occupation = request.form.get("occupation")
+        date = request.form.get("date")
+        print(first_name, last_name, email, occupation, date)
+        return render_template("index.html", first_name=first_name,
+                               last_name=last_name,
+                               email=email, occupation=occupation, date=date)
+    else:
+        pass
     return render_template("index.html")
 
 
