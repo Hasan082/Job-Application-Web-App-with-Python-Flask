ef093c80390914ecdb2d19a7028204c6df9e9808
Md Hasanuzzaman <dr.has82@gmail.com>
2024-06-03 00:54:47 +0600

    [ADDED] Db created and submit form data to database

--- app.py
+++ app.py
@@ -1,6 +1,22 @@
+from datetime import datetime
 from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+app.config["SECRET_KEY"] = "jobAplication"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+db = SQLAlchemy(app)
+
+
+class formData(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(100), nullable=False)
+    last_name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), nullable=False)
+    occupation = db.Column(db.String(50), nullable=False)
+    date = db.Column(db.Date)
+    
 
 
 @app.route("/", methods=["GET", "POST"])
@@ -11,13 +27,21 @@ def index():
         email = request.form.get("email")
         occupation = request.form.get("occupation")
         date = request.form.get("date")
-        print(first_name, last_name, email, occupation, date)
-        return render_template("index.html", first_name=first_name,
-                               last_name=last_name,
-                               email=email, occupation=occupation, date=date)
-    else:
-        pass
+        date_obj = datetime.strptime(date, "%Y-%m-%d")
+        
+        form = formData(
+            first_name=first_name,
+            last_name=last_name,
+            email=email,
+            occupation=occupation,
+            date=date_obj,
+        )
+        db.session.add(form)
+        db.session.commit()
     return render_template("index.html")
 
 
-app.run(debug=True, port=5000)
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+        app.run(debug=True, port=5000)
